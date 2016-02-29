7a39c557b76e6f22ad6c5a79fbee24f42c4bfc8e
Elena Pochernina <elenap@mstlab.org>
2016-02-29 14:55:06 +0300

    add fallbackPlot

--- src/viewer/Fallback.ts
+++ src/viewer/Fallback.ts
@@ -8,10 +8,21 @@ declare var Microsoft: any;
 module ChartViewer {
     PlotRegistry["fallback"] = {
         initialize(plotDefinition: PlotInfo, viewState: ViewState, chart: IDDPlot) {
-            return undefined;
+            var div = $("<div></div>")
+                .attr("data-idd-name", plotDefinition.displayName)
+                .appendTo(chart.host);
+            var plot = new FallbackPlot(div, chart.master);
+            chart.addChild(plot);
+            return [plot];
         },
 
         draw(plots: IDDPlot[], plotDefinition: PlotInfo) {        
+            var drawArgs = {
+                kind: plotDefinition.kind,
+                error: plotDefinition["error"]
+            }
+            plots[0].draw(drawArgs);
+            
         },
 
         createPlotCardContent: function (plotInfo) {
@@ -31,4 +42,78 @@ module ChartViewer {
             }
         }
     }
+
+    export function FallbackPlot(div, master) {
+        var that = this;
+
+        // Initialization (#1)
+        var initializer = InteractiveDataDisplay.Utils.getDataSourceFunction(div, InteractiveDataDisplay.readCsv);
+        var initialData = initializer(div);
+
+        this.base = InteractiveDataDisplay.CanvasPlot;
+        this.base(div, master);
+
+        var _kind;
+        var _error;
+        if (initialData) _kind = initialData.kind;
+        this.draw = function (data) {
+            _kind = data.kind;
+            _error = data.error;
+            this.fireAppearanceChanged('error');
+            
+            
+        };
+        // Returns 4 margins in the screen coordinate system
+        this.getLocalPadding = function () {
+            return { left: 0, right: 0, top: 0, bottom: 0 };
+        };
+
+        this.renderCore = function (plotRect, screenSize) {
+        };
+
+        this.getLegend = function () {
+            var div = $("<div class='idd-legend-item'></div>");
+
+            var canvas = $("<canvas style='margin-right: 15px'></canvas>").appendTo(div);
+            canvas.prop({ width: 20, height: 20 });
+            var ctx = (<HTMLCanvasElement>canvas.get(0)).getContext("2d");
+           
+            var that = this;
+            var nameDiv = $("<span class='idd-legend-item-title'></span>").appendTo(div);
+            var contentDiv = $("<div class='plotcard-error'></div>").appendTo(div);
+            var setName = function () {
+                nameDiv.text(that.name);
+            }
+            setName();
+            var content = "";
+            var setContent = function () {
+                var content = "";
+                if (_error) content = _error;
+                else if (_kind) content = 'kind "' + _kind + '" is unknown';
+                else content = "Error plot definition!";
+                contentDiv.text(content);
+            }
+            setContent();
+
+            this.host.bind("appearanceChanged",
+                function (event, propertyName) {
+                    if (!propertyName || propertyName == "error")
+                        setContent();
+                    if (!propertyName || propertyName == "name")
+                        setName();
+                });
+
+            var that = this;
+
+            var onLegendRemove = function () {
+                that.host.unbind("appearanceChanged");
+
+                div[0].innerHTML = "";
+                div.removeClass("idd-legend-item");
+            };
+
+            return { div: div, onLegendRemove: onLegendRemove };
+        };
+    }
+    FallbackPlot.prototype = new InteractiveDataDisplay.CanvasPlot;
 }
