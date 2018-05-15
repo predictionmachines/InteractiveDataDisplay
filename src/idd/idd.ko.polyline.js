56aa58800a8861dc42e1c91cbc6d8265f3977a49
LucyMu <lucymu@redshiftlab.ru>
2018-05-15 11:25:51 +0300

    custom binding for plot.order added; order binding sample page added

--- src/idd/idd.ko.polyline.js
+++ src/idd/idd.ko.polyline.js
@@ -1,16 +1,16 @@
 
-(function(InteractiveDataDisplay) {
+(function (InteractiveDataDisplay) {
     if (!ko) {
         console.log("Knockout was no found, please load Knockout first");
     } else {
-        var updatePolyline = function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var updatePolyline = function (element, valueAccessor, allBindings, viewModel, bindingContext) {
             var data = {};
             if (!allBindings.has('iddY'))
                 if (!allBindings.has('iddYMedian'))
                     throw new Error("Please define iddY or iddYMedian binding along with iddX");
                 else {
-                    data.y = {median: ko.unwrap(allBindings.get('iddYMedian'))};
-                    
+                    data.y = { median: ko.unwrap(allBindings.get('iddYMedian')) };
+
                     if (allBindings.has('iddLower68'))
                         data.y.lower68 = ko.unwrap(allBindings.get('iddLower68'));
                     if (allBindings.has('iddUpper68'))
@@ -22,16 +22,16 @@
                 }
             else
                 data.y = ko.unwrap(allBindings.get('iddY'));
-            
+
             if (!allBindings.has('iddX'))
                 throw new Error("Please define iddX binding along with iddY");
             else
                 data.x = ko.unwrap(allBindings.get('iddX'));
-            
+
             var n;
             if (Array.isArray(data.x))
                 n = data.x.length;
-            else throw new Error("iddX is not array");
+            else throw new Error("iddX is not an array");
 
             if (Array.isArray(data.y)) {
                 if (data.y.length !== n)
@@ -46,22 +46,22 @@
                 if (Array.isArray(data.y.lower95) && data.y.lower95.length !== n)
                     return;
                 if (Array.isArray(data.y.upper95) && data.y.upper95.length !== n)
-                    return;            
+                    return;
             }
-            
-            if (allBindings.has('iddStroke')) 
+
+            if (allBindings.has('iddStroke'))
                 data.stroke = ko.unwrap(allBindings.get('iddStroke'));
-            if (allBindings.has('iddThickness')) 
+            if (allBindings.has('iddThickness'))
                 data.thickness = ko.unwrap(allBindings.get('iddThickness'));
-            if (allBindings.has('iddLineCap')) 
+            if (allBindings.has('iddLineCap'))
                 data.lineCap = ko.unwrap(allBindings.get('iddLineCap'));
-            if (allBindings.has('iddLineJoin')) 
+            if (allBindings.has('iddLineJoin'))
                 data.lineJoin = ko.unwrap(allBindings.get('iddLineJoin'));
-            if (allBindings.has('iddFill68')) 
+            if (allBindings.has('iddFill68'))
                 data.fill68 = ko.unwrap(allBindings.get('iddFill68'));
-            if (allBindings.has('iddFill95')) 
+            if (allBindings.has('iddFill95'))
                 data.fill95 = ko.unwrap(allBindings.get('iddFill95'));
-            
+
             var plotAttr = element.getAttribute("data-idd-plot");
             if (plotAttr != null) {
                 if (typeof element.plot != 'undefined') {
@@ -75,7 +75,7 @@
         }
 
         InteractiveDataDisplay.KnockoutBindings.registerPlotBinding("polyline", updatePolyline, ['iddX', 'iddY', 'iddYMedian', 'iddLower68', 'iddUpper68', 'iddLower95',
-                                                                                                 'iddUpeer95', 'iddFill68', 'iddFill95', 'iddStroke', 'iddThickness',
-                                                                                                 'iddLineCap', 'iddLineJoin'])
+            'iddUpeer95', 'iddFill68', 'iddFill95', 'iddStroke', 'iddThickness',
+            'iddLineCap', 'iddLineJoin'])
     }
 })(InteractiveDataDisplay || (InteractiveDataDisplay = {}))
